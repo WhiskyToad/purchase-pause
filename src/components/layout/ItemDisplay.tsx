62f491e31d2606e323da4a750ecabe5c17eefde3
Steven Craig <stevencraig321@gmail.com>
2024-02-26 20:42:15 +0000

    feat: buttons on item display

--- src/components/layout/ItemDisplay.tsx
+++ src/components/layout/ItemDisplay.tsx
@@ -1,8 +1,9 @@
 import { useTheme } from "@/contexts/ThemeContext";
 import { useState } from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import EditItemModal from "./modals/EditItemModal";
 import type { PurchaseItem } from "@/types/item.types";
+import CustomButton from "../ui/CustomButton";
 
 type ItemDisplayProps = {
   item: PurchaseItem;
@@ -17,29 +18,42 @@ const ItemDisplay = (props: ItemDisplayProps) => {
   };
 
   return (
-    <TouchableOpacity
-      onPress={toggleModal}
-      style={{
-        padding: 10,
-        backgroundColor: theme.backgroundColor,
-        borderColor: theme.textColor,
-        borderWidth: 2,
-        borderRadius: 4,
-        width: "100%",
-      }}
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: theme.backgroundColor,
+          borderColor: theme.textColor,
+        },
+      ]}
     >
       <Text style={{ color: theme.textColor, fontSize: 16, marginBottom: 5 }}>
         {props.item.itemName}
       </Text>
       <Text style={{ color: theme.textColor }}>Cost: {props.item.cost}</Text>
       <Text style={{ color: theme.textColor }}>Days Left: {1}</Text>
-      <EditItemModal
-        visibile={showEditModal}
-        toggleModal={toggleModal}
-        item={props.item}
-      />
-    </TouchableOpacity>
+
+      <CustomButton onPress={toggleModal} variant="primary" text="Edit" />
+      <CustomButton onPress={() => {}} variant="secondary" text="Purchased" />
+
+      {showEditModal && (
+        <EditItemModal
+          visibile={showEditModal}
+          toggleModal={toggleModal}
+          item={props.item}
+        />
+      )}
+    </View>
   );
 };
 
 export default ItemDisplay;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 4,
+    width: "100%",
+  },
+});
