dc52582ef41dfeb35e46c7913d126cc0bc3287a6
Steven Craig <stevencraig321@gmail.com>
2024-03-07 08:09:03 +0000

    feat: logic for working out daily notifications

--- src/hooks/useNotificationSettings.tsx
+++ src/hooks/useNotificationSettings.tsx
@@ -1,8 +1,10 @@
+import { useNullStatusItemContext } from "@/contexts/NullStatusItemsContext";
 import { useSettings } from "@/contexts/SettingsContext";
 import { useEffect } from "react";
 
 const useNotificationSettings = () => {
   const { settings } = useSettings();
+  const { items: purchaseItems } = useNullStatusItemContext();
 
   useEffect(() => {
     if (!settings) return;
@@ -10,8 +12,20 @@ const useNotificationSettings = () => {
 
     if (notificationsEnabled) {
       if (notificationsFrequency === "daily") {
-        // Logic for triggering daily notifications
-        console.log("Trigger daily notification");
+        const currentDate = new Date();
+
+        // Filter purchase items that are not completed and were created more than 24 hours ago
+        const eligibleItems = purchaseItems.filter(
+          (item) =>
+            !item.completedAt &&
+            currentDate.getTime() - new Date(item.createdAt).getTime() >=
+              24 * 60 * 60 * 1000
+        );
+
+        console.log(
+          `Trigger daily notification for items: ${eligibleItems.length}`
+        );
+        // Your notification logic here...
       } else if (notificationsFrequency === "weekly") {
         // Logic for triggering weekly notifications
         console.log("Trigger weekly notification");
