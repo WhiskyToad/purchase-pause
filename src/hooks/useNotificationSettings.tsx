53b462ae15a8d437d34c5a6144fae42606f94889
Steven Craig <stevencraig321@gmail.com>
2024-03-07 08:02:44 +0000

    feat: skeleton for daily and weekly notifications

--- src/hooks/useNotificationSettings.tsx
+++ src/hooks/useNotificationSettings.tsx
@@ -1,3 +1,23 @@
-const useNotificationSettings = () => {};
+import { useSettings } from "@/contexts/SettingsContext";
+import { useEffect } from "react";
+
+const useNotificationSettings = () => {
+  const { settings } = useSettings();
+
+  useEffect(() => {
+    if (!settings) return;
+    const { notificationsEnabled, notificationsFrequency } = settings;
+
+    if (notificationsEnabled) {
+      if (notificationsFrequency === "daily") {
+        // Logic for triggering daily notifications
+        console.log("Trigger daily notification");
+      } else if (notificationsFrequency === "weekly") {
+        // Logic for triggering weekly notifications
+        console.log("Trigger weekly notification");
+      }
+    }
+  }, [settings]);
+};
 
 export default useNotificationSettings;
