862fc102ed704f127b56a8f5df62f7ac88575c14
Steven Craig <stevencraig321@gmail.com>
2024-03-12 19:08:33 +0000

    feat: notifications scheduled

--- src/hooks/useNotificationSettings.tsx
+++ src/hooks/useNotificationSettings.tsx
@@ -1,37 +1,71 @@
 import { useNullStatusItemContext } from "@/contexts/NullStatusItemsContext";
 import { useSettings } from "@/contexts/SettingsContext";
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
+import * as Notifications from "expo-notifications";
 
 const useNotificationSettings = () => {
   const { settings } = useSettings();
   const { items: purchaseItems } = useNullStatusItemContext();
 
+  const eligibleItems = useMemo(() => {
+    const currentDate = new Date();
+    purchaseItems.filter(
+      (item) =>
+        !item.completedAt &&
+        currentDate.getTime() - new Date(item.createdAt).getTime() >=
+          24 * 60 * 60 * 1000
+    );
+  }, [purchaseItems]);
+  const scheduleNotification = useCallback(async (trigger: object) => {
+    try {
+      if (!settings) return;
+      const { notificationsEnabled } = settings;
+
+      if (notificationsEnabled) {
+        // Check permission status
+        const { status } = await Notifications.getPermissionsAsync();
+        if (status !== "granted") {
+          console.log("Notification permission not granted");
+          return;
+        }
+
+        // Construct notification content
+        const notificationContent = {
+          title: "Pause, Reflect, Remember",
+          body: `You have ${eligibleItems} items on pause`,
+        };
+
+        // Schedule the notification
+        await Notifications.scheduleNotificationAsync({
+          content: notificationContent,
+          trigger,
+        });
+      }
+    } catch (error) {
+      console.error("Error scheduling notification:", error);
+    }
+  }, []);
+
   useEffect(() => {
     if (!settings) return;
-    const { notificationsEnabled, notificationsFrequency } = settings;
 
-    if (notificationsEnabled) {
-      if (notificationsFrequency === "daily") {
-        const currentDate = new Date();
+    const { notificationsFrequency } = settings;
 
-        // Filter purchase items that are not completed and were created more than 24 hours ago
-        const eligibleItems = purchaseItems.filter(
-          (item) =>
-            !item.completedAt &&
-            currentDate.getTime() - new Date(item.createdAt).getTime() >=
-              24 * 60 * 60 * 1000
-        );
-
-        console.log(
-          `Trigger daily notification for items: ${eligibleItems.length}`
-        );
-        // Your notification logic here...
-      } else if (notificationsFrequency === "weekly") {
-        // Logic for triggering weekly notifications
-        console.log("Trigger weekly notification");
-      }
+    if (notificationsFrequency === "daily") {
+      scheduleNotification({
+        hour: 10,
+        minute: 30,
+        repeats: true,
+      });
+    } else if (notificationsFrequency === "weekly") {
+      scheduleNotification({
+        weekday: 1, // Monday
+        hour: 10, // Adjust this to the desired time for the weekly notification
+        minute: 30,
+        repeats: true, // Set to true for weekly repetition
+      });
     }
-  }, [settings]);
+  }, [settings, scheduleNotification]);
 };
 
 export default useNotificationSettings;
